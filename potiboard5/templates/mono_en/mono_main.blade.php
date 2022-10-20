6a2bec3436e4bc913515782e4d3356d5f2b6732e
satopian <satopian@mia.eek.jp>
2022-10-20 22:55:15 +0900

    Tweet

--- potiboard5/templates/mono_en/mono_main.blade.php
+++ potiboard5/templates/mono_en/mono_main.blade.php
@@ -342,11 +342,11 @@
 				<span class="share_button">
 					<a target="_blank"
 						href="https://twitter.com/intent/tweet?&text=%5B{{$ress[0]['encoded_no']}}%5D%20{{$ress[0]['share_sub']}}%20by%20{{$ress[0]['share_name']}}%20-%20{{$encoded_title}}&url={{$encoded_rooturl}}{{$encoded_self}}?res={{$ress[0]['encoded_no']}}"><span
-							class="button"><img src="{{$skindir}}img/twitter.svg" alt=""> tweet</span></a>
+							class="button"><img src="{{$skindir}}img/twitter.svg" alt=""> Tweet</span></a>
 					<a target="_blank" class="fb btn"
 						href="http://www.facebook.com/share.php?u={{$encoded_rooturl}}{{$encoded_self}}?res={{$ress[0]['encoded_no']}}"><span
 							class="button"><img src="{{$skindir}}img/facebook.svg" alt="">
-							share</span></a>
+							Share</span></a>
 				</span>
 				@endif
 				@if($notres)<span class="button"><a href="{{$self}}?res={{$ress[0]['no']}}"><img
