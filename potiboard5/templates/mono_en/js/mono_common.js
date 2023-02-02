7f8b14433ecaa590e0f84e06f5d691a848dc92bf
satopian <satopian@mia.eek.jp>
2023-02-02 19:02:42 +0900

    mono_common.js if(commentform.name){

--- potiboard5/templates/mono_en/js/mono_common.js
+++ potiboard5/templates/mono_en/js/mono_common.js
@@ -38,55 +38,67 @@
 		document.getElementById("mystyle").selectedIndex = colorIdx;
 	};
 
-jQuery(function() {
-	window.onpageshow = function () {
-		var $btn = $('[type="submit"]');
-		//disbledを解除
-		$btn.prop('disabled', false);
-		$btn.click(function () { //送信ボタン2度押し対策
-			$(this).prop('disabled', true);
-			$(this).closest('form').submit();
+	jQuery(function() {
+		window.onpageshow = function () {
+			var $btn = $('[type="submit"]');
+			//disbledを解除
+			$btn.prop('disabled', false);
+			$btn.click(function () { //送信ボタン2度押し対策
+				$(this).prop('disabled', true);
+				$(this).closest('form').submit();
+			});
+		}
+		// https://cotodama.co/pagetop/
+		var pagetop = $('#page_top');   
+		pagetop.hide();
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
+				pagetop.fadeIn();
+			} else {
+				pagetop.fadeOut();
+			}
 		});
-	}
-	// https://cotodama.co/pagetop/
-	var pagetop = $('#page_top');   
-	pagetop.hide();
-	$(window).scroll(function () {
-		if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
-			pagetop.fadeIn();
-		} else {
-			pagetop.fadeOut();
-		}
-	});
-	pagetop.click(function () {
-		$('body,html').animate({
-			scrollTop: 0
-		}, 500); //0.5秒かけてトップへ移動
-		return false;
-	});
-	// https://www.webdesignleaves.com/pr/plugins/luminous-lightbox.html
-	const luminousElems = document.querySelectorAll('.luminous');
-	//取得した要素の数が 0 より大きければ
-	if( luminousElems.length > 0 ) {
-		luminousElems.forEach( (elem) => {
-		new Luminous(elem);
+		pagetop.click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 500); //0.5秒かけてトップへ移動
+			return false;
 		});
-	}
-	const paintform = document.getElementById("paint_form");
-	if(paintform){
-		paintform.onsubmit = function (){
-		SetCookie("picwc",paintform.picw.value);
-		SetCookie("pichc",paintform.pich.value);
-		SetCookie("appletc",paintform.shi.value);
+		// https://www.webdesignleaves.com/pr/plugins/luminous-lightbox.html
+		const luminousElems = document.querySelectorAll('.luminous');
+		//取得した要素の数が 0 より大きければ
+		if( luminousElems.length > 0 ) {
+			luminousElems.forEach( (elem) => {
+			new Luminous(elem);
+			});
 		}
-	};
-	const commentform = document.getElementById("comment_form");
-	if(commentform){
-		commentform.onsubmit = function (){
-		SetCookie("namec",commentform.name.value);
-		SetCookie("urlc",commentform.url.value);
-		SetCookie("pwdc",commentform.pwd.value);
-		}
-	};
-});
+		const paintform = document.getElementById("paint_form");
+		if(paintform){
+			paintform.onsubmit = function (){
+				if(paintform.picw){
+					SetCookie("picwc",paintform.picw.value);
+				}
+				if(paintform.pich){
+					SetCookie("pichc",paintform.pich.value);
+				}
+				if(paintform.shi){
+					SetCookie("appletc",paintform.shi.value);
+				}
+			}
+		};
+		const commentform = document.getElementById("comment_form");
+		if(commentform){
+			commentform.onsubmit = function (){
+				if(commentform.name){
+					SetCookie("namec",commentform.name.value);
+				}
+				if(commentform.url){
+					SetCookie("urlc",commentform.url.value);
+				}
+				if(commentform.pwd){
+					SetCookie("pwdc",commentform.pwd.value);
+				}
+			}
+		};
+	});
 
