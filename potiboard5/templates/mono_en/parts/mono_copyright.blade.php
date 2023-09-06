5e1905cbd0150c977f7481657c2615a0c14ee75f
satopian <satopian@mia.eek.jp>
2023-09-06 21:36:46 +0900

    mono_copyright.blade.php

--- potiboard5/templates/mono_en/parts/mono_copyright.blade.php
+++ potiboard5/templates/mono_en/parts/mono_copyright.blade.php
@@ -2,29 +2,29 @@
 {{--  Copyright notice, do not delete  --}}
 <!-- GazouBBS v3.0 --><!-- futaba Kai 0.8 --><!-- POTI-board -->
 				<p>
-		<a href="https://paintbbs.sakura.ne.jp/poti/" target="_blank" rel="nofollow noopener noreferrer" title="POTI-board EVO {{$verlot}}">POTI-board EVO {{$ver}}</a>
-		Web Style by <a href="https://paintbbs.sakura.ne.jp/poti/" target="_blank" rel="nofollow noopener noreferrer" title="MONO {{$tver}}">MONO</a>
+		<a href="https://paintbbs.sakura.ne.jp/poti/" target="_blank" rel="noopener noreferrer" title="POTI-board EVO {{$verlot}}">POTI-board EVO {{$ver}}</a>
+		Web Style by <a href="https://paintbbs.sakura.ne.jp/poti/" target="_blank" rel="noopener noreferrer" title="MONO {{$tver}}">MONO</a>
 	</p>
 	<p>
 		OriginalScript -
-		<a href="http://www.punyu.net/" target="_blank" rel="nofollow noopener noreferrer" title="POTI-board (by PunyuNet)">POTI-board</a>
+		<a href="http://www.punyu.net/" target="_blank" rel="noopener noreferrer" title="POTI-board (by PunyuNet)">POTI-board</a>
 		<span title="futaba.php v0.8 lot.031015 (by futaba)">futaba.php</span>
-		(<a href="http://php.loglog.jp/" target="_blank" rel="nofollow noopener noreferrer" title="gazou.php v3.0 (by Let's PHP!)">gazou.php</a>custom )
+		(<a href="http://php.loglog.jp/" target="_blank" rel="noopener noreferrer" title="gazou.php v3.0 (by Let's PHP!)">gazou.php</a>custom )
 	</p>
 	@if($paint)
 	<p>
 		OekakiApp - <a href="http://hp.vector.co.jp/authors/VA016309/" target="_blank" rel="noopener noreferrer" title="by しぃちゃん">Shi-Painter</a>
-		<a href="https://github.com/funige/neo/" target="_blank" rel="nofollow noopener noreferrer" title="by funige">PaintBBS NEO</a>
+		<a href="https://github.com/funige/neo/" target="_blank" rel="noopener noreferrer" title="by funige">PaintBBS NEO</a>
 		<a href="https://github.com/desuwa/tegaki" target="_blank" rel="noopener noreferrer" title="by Maxime Youdine">Tegaki</a> 
 		<a href="https://github.com/bitbof/klecks"  target="_blank" rel="noopener noreferrer" title="by bitbof">klecks</a>
-		<a href="https://github.com/thenickdude/chickenpaint" target="_blank" rel="nofollow noopener noreferrer" title="by Nicholas Sherlock">ChickenPaint</a>
+		<a href="https://github.com/thenickdude/chickenpaint" target="_blank" rel="noopener noreferrer" title="by Nicholas Sherlock">ChickenPaint</a>
 	</p>
 	@endif
 	<p>
 		UseFunction -
 		<span title="by WonderCatStudio">DynamicPalette</span> <!-- http://wondercatstudio.com/ -->
 		<a href="https://github.com/imgix/luminous" target="_blank" rel="noopener noreferrer" title="by Zebrafish Labs" >luminous</a>
-		<a href="https://github.com/EFTEC/BladeOne" target="_blank" rel="nofollow noopener noreferrer" title="BladeOne">BladeOne</a>
+		<a href="https://github.com/EFTEC/BladeOne" target="_blank" rel="noopener noreferrer" title="BladeOne">BladeOne</a>
 	</p>
 </div>
 	
