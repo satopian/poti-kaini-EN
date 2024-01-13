e94d3ef68711a6d90062de5342921da88c69448b
satopian <satopian@mia.eek.jp>
2024-01-13 09:30:48 +0900

    v6.18.9 シフトキー+左ドラッグでブラシサイズと不透明度のスライダーを緩やかに調整。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.18.8';
-const POTI_LOT = 'lot.20240110';
+const POTI_VER = 'v6.18.9';
+const POTI_LOT = 'lot.20240112';
 
 /*
   (C) 2018-2023 POTI改 POTI-board redevelopment team
