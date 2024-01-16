32aff49094c9c86412940adb8bcf68b06743fa33
satopian <satopian@mia.eek.jp>
2024-01-16 14:12:03 +0900

    chickenpaint.js

--- potiboard5/chickenpaint/js/chickenpaint.js
+++ potiboard5/chickenpaint/js/chickenpaint.js
@@ -26657,6 +26657,9 @@ module.exports={
   "Whoops! You can't draw on a group": "グループフォルダには描けません",
   "Whoops! All of the selected pixels are transparent!": "レイヤーが空です",
  
+  "Image": "画像",
+  "Layer mask": "レイヤーマスク",
+
   "Sorry, you can't remove the last remaining layer in the drawing.": "レイヤーが1つしか残っていないため削除できません。",
  
   "Are you sure you want to send your drawing to the server and finish drawing now?": "お絵かきをサーバーに送信します。終了してもよろしいですか？",
