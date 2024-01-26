80a37371a19add53d26506a58eb6a306f4a6b0a4
satopian <satopian@mia.eek.jp>
2024-01-27 00:00:38 +0900

    v6.21.0 ユーザーコードの取得を拡張ヘッダから、GETによる取得に変更。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.20.5';
-const POTI_LOT = 'lot.20240125';
+const POTI_VER = 'v6.21.0';
+const POTI_LOT = 'lot.20240126';
 
 /*
   (C) 2018-2023 POTI改 POTI-board redevelopment team
