97a99cc007ce41b57afce70d2454c8b143f39505
satopian <satopian@mia.eek.jp>
2024-02-08 14:02:04 +0900

    neo.js pchを送信する条件を、ヘッダテキスト、thumbnail、画像、pchのファイルサイズの合計との比較に変更。

--- potiboard5/neo.js
+++ potiboard5/neo.js
@@ -1165,16 +1165,17 @@ Neo.submit = function (board, blob, thumbnail, thumbnail2) {
   }
 
   if (Neo.config.neo_send_with_formdata == "true") {
-    var formData = new FormData();
-    formData.append('header', headerString);
-	  formData.append('picture',blob,blob);
-
-	  if (thumbnail) {
-		  formData.append('thumbnail',thumbnail,blob);
-	  }
-	  if (thumbnail2 && blob) {
-		if (typeof Neo.config.neo_max_pch === 'undefined'||!Neo.config.neo_max_pch||((parseInt(Neo.config.neo_max_pch)*1024*1024) > (blob.size+thumbnail2.size))) {
-		  formData.append('pch',thumbnail2,blob);
+	var formData = new FormData();
+	formData.append('header', headerString);
+	formData.append('picture',blob,blob);
+	let thumbnail_size = 0;
+	if (thumbnail) {
+		formData.append('thumbnail',thumbnail,blob);
+		thumbnail_size=thumbnail.size;
+	}
+	if (thumbnail2) {
+	  if (!Neo.config.neo_max_pch || isNaN(parseInt(Neo.config.neo_max_pch)) || ((parseInt(Neo.config.neo_max_pch)*1024*1024) > (headerString.length+blob.size+thumbnail_size+thumbnail2.size))) {
+		formData.append('pch',thumbnail2,blob);
 		}
 	  }
   }
