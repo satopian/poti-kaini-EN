a23cbfdca03c2416212d77792f4536d5284275eb
satopian <satopian@mia.eek.jp>
2024-05-28 16:43:39 +0900

    v6.31.9 ChickenPaintのレイヤーパレットの操作アイコンが横並び1列に収まるように調整。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.31.8';
-const POTI_LOT = 'lot.20240526';
+const POTI_VER = 'v6.31.9';
+const POTI_LOT = 'lot.20240528';
 
 /*
   (C) 2018-2023 POTI改 POTI-board redevelopment team
