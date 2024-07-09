8ccb1372a2e8bcc440eae543a2edaba03ad74928
satopian <satopian@mia.eek.jp>
2024-07-09 20:04:36 +0900

    v6.32.8

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.32.7';
-const POTI_LOT = 'lot.20240705';
+const POTI_VER = 'v6.32.8';
+const POTI_LOT = 'lot.20240709';
 
 /*
   (C) 2018-2023 POTI改 POTI-board redevelopment team
