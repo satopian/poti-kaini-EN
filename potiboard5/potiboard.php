83b9588eb0dfd6aa2e20284eba98079bc4a8bb8d
satopian <satopian@mia.eek.jp>
2024-11-11 14:49:48 +0900

    htaccess更新。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.39.11';
-const POTI_LOT = 'lot.20241108';
+const POTI_VER = 'v6.39.12';
+const POTI_LOT = 'lot.20241111';
 
 /*
   (C) 2018-2023 POTI改 POTI-board redevelopment team
