6010f17686f1e45841a426b189f01bcdafb18aa1
satopian <satopian@mia.eek.jp>
2025-01-15 00:29:18 +0900

    NEO更新。描画アニメーションをスキップして続きを描く時にアンドゥするとキャンバスが空白になったままになる問題を修正。

--- potiboard5/neo.js
+++ potiboard5/neo.js
@@ -6342,6 +6342,7 @@ Neo.ActionManager.prototype.play = function () {
 
     if (Neo.painter.busySkipped) {
       Neo.painter.busySkipped = false;
+      Neo.painter.dirty = true;
       console.log("animation skipped");
     } else {
       console.log("animation finished");
@@ -6351,7 +6352,7 @@ Neo.ActionManager.prototype.play = function () {
 
   var item = this._items[this._head];
 
-  if (!Neo.viewer) {
+  if (!Neo.viewer && !Neo.painter.busySkipped) {
     Neo.painter._pushUndo(
       0,
       0,
