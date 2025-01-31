39316e74e1e687db3f1b5cfe112a9334c4fcff3e
satopian <satopian@mia.eek.jp>
2025-01-31 22:55:42 +0900

    pchダウンロードリンクを検索エンジンのクロールの対象にしない。rel="nofollow noopener noreferrer"

--- potiboard5/templates/mono_en/mono_paint.blade.php
+++ potiboard5/templates/mono_en/mono_paint.blade.php
@@ -671,7 +671,7 @@
 						@endif
 					</div>
 					<p>
-						<a href="{{$pchfile}}" target="_blank">Download</a> - Datasize : {{$datasize}} KB
+						<a href="{{$pchfile}}" target="_blank" rel="nofollow noopener noreferrer">Download</a> - Datasize : {{$datasize}} KB
 					</p>
 					<p>
 						<a href="javascript:close()">close</a>
