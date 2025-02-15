28ab5657b1150db0393c1db6f9b717bb32c088b3
satopian <satopian@mia.eek.jp>
2025-02-15 15:13:47 +0900

    klecks

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.65.2';
-const POTI_LOT = 'lot.20250212';
+const POTI_VER = 'v6.65.3';
+const POTI_LOT = 'lot.20250215';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
