35be869e858fbf7fe2cd3e9ca849332528d7a61d
satopian <satopian@mia.eek.jp>
2025-03-06 21:16:21 +0900

    loadcookie.js

--- potiboard5/loadcookie.js
+++ potiboard5/loadcookie.js
@@ -1,78 +1,72 @@
-function l(){
-	var P=loadCookie("pwdc"),N=loadCookie("namec"),E=loadCookie("emailc"),U=loadCookie("urlc"),
-	FC=loadCookie("fcolorc"),AP=loadCookie("appletc"),PW=loadCookie("picwc"),PH=loadCookie("pichc"),PL=loadCookie("palettec"),i;
-		for(i=0;i<document.forms.length;i++){
-			if(document.forms[i].pwd){
-				document.forms[i].pwd.value=P;
-			}
-			if(document.forms[i].name){
-				document.forms[i].name.value=N;
-			}
-			if(document.forms[i].email){
-				document.forms[i].email.value=E;
-			}
-			if(document.forms[i].url){
-				document.forms[i].url.value=U;
-			}
-			if(document.forms[i].fcolor){
-				if(FC == "") FC = document.forms[i].fcolor[0].value;
+"usestrict";
+function l() {
+    const P = loadCookie("pwdc"),
+        N = loadCookie("namec"),
+        E = loadCookie("emailc"),
+        U = loadCookie("urlc"),
+        FC = loadCookie("fcolorc"),
+        AP = loadCookie("appletc"),
+        PW = loadCookie("picwc"),
+        PH = loadCookie("pichc"),
+        PL = loadCookie("palettec");
+    for (let i = 0; i < document.forms.length; i++) {
+        const form = document.forms[i];
 
-				checkd_if_formval_equal_cookieval(document.forms[i].fcolor,FC);
+        if (form.pwd) {
+            form.pwd.value = P;
+        }
+        if (form.name) {
+            form.name.value = N;
+        }
+        if (form.email) {
+            form.email.value = E;
+        }
+        if (form.url) {
+            form.url.value = U;
+        }
+        if (form.fcolor) {
+            if (FC == "") FC = form.fcolor[0].value;
 
-			}
-			if(document.forms[i].shi){
+            checkd_if_formval_equal_cookieval(form.fcolor, FC);
+        }
+        if (form.shi) {
+            checkd_if_formval_equal_cookieval(form.shi, AP);
+        }
+        if (form.picw) {
+            if (PW != "") {
+                form.picw.value = PW;
+            }
 
-				checkd_if_formval_equal_cookieval(document.forms[i].shi,AP);
+            checkd_if_formval_equal_cookieval(form.picw, PW);
+        }
 
-			}
-			if(document.forms[i].picw){
-				if(PW != ""){
-					document.forms[i].picw.value=PW;
-				}
+        if (form.pich) {
+            if (PH != "") {
+                form.pich.value = PH;
+            }
 
-				checkd_if_formval_equal_cookieval(document.forms[i].picw,PW);
+            checkd_if_formval_equal_cookieval(form.pich, PH);
+        }
 
-			}
-
-			if(document.forms[i].pich){
-				if(PH != ""){
-					document.forms[i].pich.value=PH;
-				}
-
-				checkd_if_formval_equal_cookieval(document.forms[i].pich,PH);
-
-			}
-
-			if(document.forms[i].selected_palette_no){
-				document.forms[i].selected_palette_no.selectedIndex = PL;
-			}
-
-		}
-};
+        if (form.selected_palette_no) {
+            form.selected_palette_no.selectedIndex = PL;
+        }
+    }
+}
 
 //Cookieと一致したらcheckd
-function checkd_if_formval_equal_cookieval(docformsname,cookieval) {
-var j;
-	for(j = 0; docformsname.length > j; j ++) {
-	if(docformsname[j].value == cookieval){
-		docformsname[j].checked = true;//チェックボックス
-		docformsname.selectedIndex = j;//プルダウンメニュー
-	}
+function checkd_if_formval_equal_cookieval(docformsname, cookieval) {
+    var j;
+    for (j = 0; docformsname.length > j; j++) {
+        if (docformsname[j].value == cookieval) {
+            docformsname[j].checked = true; //チェックボックス
+            docformsname.selectedIndex = j; //プルダウンメニュー
+        }
+    }
 }
-}
-
-/* Function to get cookie parameter value string with specified name
-   Copyright (C) 2002 Cresc Corp. http://www.cresc.co.jp/
-   Version: 1.0
-*/
+//指定した名前のCookieをロード
 function loadCookie(name) {
-	var allcookies = document.cookie;
-	if (allcookies == "") return "";
-	var start = allcookies.indexOf(name + "=");
-	if (start == -1) return "";
-	start += name.length + 1;
-	var end = allcookies.indexOf(';',start);
-	if (end == -1) end = allcookies.length;
-	
-	return decodeURIComponent(allcookies.substring(start,end));
+    const cookies = new URLSearchParams(document.cookie.replace(/; /g, "&"));
+    const cookieValue = cookies.get(name);
+    return cookieValue ? decodeURIComponent(cookieValue) : "";
 }
