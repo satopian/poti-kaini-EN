0e57e206d31718f6a767920edb26aba481de858c
satopian <satopian@mia.eek.jp>
2025-03-20 14:00:23 +0900

    Paint画面の動的パレットとアプレットの位置関係を修正。テンプレートMONOのみ。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.68.5';
-const POTI_LOT = 'lot.20250318';
+const POTI_VER = 'v6.68.6';
+const POTI_LOT = 'lot.20250320';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
