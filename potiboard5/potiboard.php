53bbffbaa813f2d28780dd3a5386fdb76b003688
satopian <satopian@mia.eek.jp>
2025-03-30 14:41:18 +0900

    ChickenPaint Be更新。レイヤーマスクをゴミ箱アイコンで削除できるようにした。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.71.0';
-const POTI_LOT = 'lot.20250327';
+const POTI_VER = 'v6.71.1';
+const POTI_LOT = 'lot.20250330';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
