b94e5687f416ef20de71c59f1edf47fcb4bd5910
satopian <satopian@mia.eek.jp>
2025-04-10 08:54:54 +0900

    neo.js　chickenpaint.js

--- potiboard5/neo.js
+++ potiboard5/neo.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 var Neo = function () {};
 
-Neo.version = "1.6.15";
+Neo.version = "1.6.16";
 Neo.painter;
 Neo.fullScreen = false;
 Neo.uploaded = false;
