0005b46922d41ac3682974ddb535848ecf5cfebb
satopian <satopian@mia.eek.jp>
2025-06-25 15:47:05 +0900

    style-switcher.blade.php

--- potiboard5/templates/mono_en/parts/style-switcher.blade.php
+++ potiboard5/templates/mono_en/parts/style-switcher.blade.php
@@ -1,13 +1,21 @@
 <style>
 	@if(!$is_IE)
-			html {
+		html {
 			visibility: hidden;
 		}
-	@endif
-		html.ie {
+	@else
+		html {
 			visibility: visible;
 		}
+	@endif
 </style>
+<noscript>
+	<style>
+	html {
+		visibility: visible;
+	}
+	</style>
+</noscript>
 <link rel="stylesheet" href="{{$skindir}}css/mono_main.css?{{$ver}}">
 <link rel="stylesheet" href="{{$skindir}}css/mono_dark.css?{{$ver}}" id="css1" disabled>
 <link rel="stylesheet" href="{{$skindir}}css/mono_deep.css?{{$ver}}" id="css2" disabled>
