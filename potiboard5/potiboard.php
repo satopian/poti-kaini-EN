b82c9388259812b4465a00f440fc82bd690dc5b9
satopian <satopian@mia.eek.jp>
2025-07-06 10:55:26 +0900

    ChickenPaint Be更新。Canvasの高さ整数、styleの高さ浮動小数点で一致せず線に段差が入る問題を修正。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.82.1';
-const POTI_LOT = 'lot.20250702';
+const POTI_VER = 'v6.83.0';
+const POTI_LOT = 'lot.20250706';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
