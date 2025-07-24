364ae08764e894b0cb2d08dd5fbfb71834fe0232
satopian <satopian@mia.eek.jp>
2025-07-24 10:54:33 +0900

    ChickenPaint Beのバグを修正。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.85.2';
-const POTI_LOT = 'lot.20250714';
+const POTI_VER = 'v6.85.3';
+const POTI_LOT = 'lot.20250724';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
