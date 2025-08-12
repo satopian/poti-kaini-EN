dbc87f27111564f1bfb4b6e7d73e32595447731c
satopian <satopian@mia.eek.jp>
2025-08-12 23:30:07 +0900

    ChickenPaint更新。表示の左右反転をワンタップでできるように。複数レイヤーがある場合でもすべて反転して表示。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.86.7';
-const POTI_LOT = 'lot.20250810';
+const POTI_VER = 'v6.87.0';
+const POTI_LOT = 'lot.20250812';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
