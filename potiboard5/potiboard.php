33f034cfda55a9d5bb2e803801356f4bd43b390a
satopian <satopian@mia.eek.jp>
2025-08-14 20:49:56 +0900

    表示の左右反転して変形、対象を回転する時にペンの回転方向と逆向きに回転するバグを修正。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.87.0';
-const POTI_LOT = 'lot.20250812';
+const POTI_VER = 'v6.87.1';
+const POTI_LOT = 'lot.20250814';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
