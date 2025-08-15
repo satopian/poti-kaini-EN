ca3c634bcea9fd03669b6fad163ce4ab27b9b2c1
satopian <satopian@mia.eek.jp>
2025-08-15 20:38:48 +0900

    長方形選択ツール選択時にツールオプションパネルに｢すべて選択｣｢選択解除｣｢変形｣ボタンを表示。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.87.1';
-const POTI_LOT = 'lot.20250814';
+const POTI_VER = 'v6.88.0';
+const POTI_LOT = 'lot.20250815';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
