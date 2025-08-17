717044be8a9e730e81c0473865d3ba11a1207072
satopian <satopian@mia.eek.jp>
2025-08-17 15:28:26 +0900

    変形ボタン青→Enterで変形。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.88.2';
-const POTI_LOT = 'lot.20250815';
+const POTI_VER = 'v6.88.3';
+const POTI_LOT = 'lot.20250817';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
