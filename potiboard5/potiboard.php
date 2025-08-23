716b0f06a325879d8a8ff10e144547b98c25ae4b
satopian <satopian@mia.eek.jp>
2025-08-23 15:58:09 +0900

    青色のボタンの色を調整。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.90.1';
-const POTI_LOT = 'lot.20250821';
+const POTI_VER = 'v6.90.2';
+const POTI_LOT = 'lot.20250823';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
