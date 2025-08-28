9dcc7ae461ebf67e4f108041c3f970f67782d38f
satopian <satopian@mia.eek.jp>
2025-08-28 13:12:09 +0900

    ChickenPaint Be更新。ポリフィル削除、ボタンの幅を150pxに余白を小さく。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.91.0';
-const POTI_LOT = 'lot.20250825';
+const POTI_VER = 'v6.92.0';
+const POTI_LOT = 'lot.20250828';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
