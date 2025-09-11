76b939aff9bbf724479c4f5207ab3b3c80057e24
satopian <satopian@mia.eek.jp>
2025-09-11 22:15:23 +0900

    テクスチャの拡大率を500%から800%へ。ポップオーバーでサムネイルが隠れないようにした。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.96.0';
-const POTI_LOT = 'lot.20250911';
+const POTI_VER = 'v6.96.1';
+const POTI_LOT = 'lot.20250912';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
