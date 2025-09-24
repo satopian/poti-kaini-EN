f0166e2eb605db44b4447591b25f8fb6f45663f3
satopian <satopian@mia.eek.jp>
2025-09-24 18:42:51 +0900

    ChickenPaint Be更新。下のレイヤーと混色。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.98.1';
-const POTI_LOT = 'lot.20250922';
+const POTI_VER = 'v6.99.1';
+const POTI_LOT = 'lot.20250924';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
