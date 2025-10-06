4df4318e4a1dec0043b63a92247d20265b734031
satopian <satopian@mia.eek.jp>
2025-10-06 18:57:00 +0900

    ChickenPaint Be更新。色収差フィルタに現在のレイヤーのみオプション、全レイヤー時は新規レイヤーを自動追加。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.101.0';
-const POTI_LOT = 'lot.20251004';
+const POTI_VER = 'v6.101.1';
+const POTI_LOT = 'lot.20251006';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
