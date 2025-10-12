414f9dc614cd7c525ff596b79b924894dec70199
satopian <satopian@mia.eek.jp>
2025-10-13 06:20:44 +0900

    ChickenPaint Be更新。レイヤーパレットの伸縮がペンや指でできなかったのを修正。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.101.3';
-const POTI_LOT = 'lot.20251011';
+const POTI_VER = 'v6.101.5';
+const POTI_LOT = 'lot.20251013';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
