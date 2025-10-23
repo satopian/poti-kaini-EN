abf091cbffdb27cd22cc2a9b9bdd40c360d499c6
satopian <satopian@mia.eek.jp>
2025-10-23 18:18:34 +0900

    ChickenPaint Be更新。｢P｣ボタンの表示色が、全パレット表示/非表示の動作と同期していなかったのを修正。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.102.1';
-const POTI_LOT = 'lot.20251022';
+const POTI_VER = 'v6.102.2';
+const POTI_LOT = 'lot.20251023';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
