05a841d8cd7ead84ce561ba37bf27d22a694808a
satopian <satopian@mia.eek.jp>
2025-11-08 17:34:41 +0900

    v6.107.1 ChickenPaint Beの選択範囲の縦横比固定ができなくなっていたのを修正。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.107.0';
-const POTI_LOT = 'lot.20251103';
+const POTI_VER = 'v6.107.1';
+const POTI_LOT = 'lot.20251108';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
