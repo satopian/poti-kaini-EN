16ae915933b4f026c3031050ac2ce9006420cec8
satopian <satopian@mia.eek.jp>
2025-12-11 08:04:26 +0900

    litaChix更新。ペンの濃さ修正。円ハードエッジ以外のブラシ先端の時に線が途切れる問題を修正。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.115.1';
-const POTI_LOT = 'lot.20251207';
+const POTI_VER = 'v6.116.1';
+const POTI_LOT = 'lot.20251211';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
