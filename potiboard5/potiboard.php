6c504d839c03e767233c677560d1c4abd269c680
satopian <satopian@mia.eek.jp>
2026-02-01 15:16:49 +0900

    litaChix更新。筆圧のサイズにローパスフィルタを追加してサイズのブレを低減。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.128.5';
-const POTI_LOT = 'lot.20260129';
+const POTI_VER = 'v6.129.1';
+const POTI_LOT = 'lot.20260201';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
