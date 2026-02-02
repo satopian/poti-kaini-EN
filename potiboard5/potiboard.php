3f6d55e9ab70a86f1c8d8db3e794ab6c3f56e93a
satopian <satopian@mia.eek.jp>
2026-02-02 23:45:58 +0900

    v1.180.0 litaChix更新。ブラシサイズを変更するショートカットキー｢CTRL+ALT+ドラッグ｣を追加。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.129.1';
-const POTI_LOT = 'lot.20260201';
+const POTI_VER = 'v6.130.0';
+const POTI_LOT = 'lot.20260202';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
