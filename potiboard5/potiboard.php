6c6c213357eeb6de04b88ae68223e1d82c947649
satopian <satopian@mia.eek.jp>
2026-02-08 16:14:19 +0900

    litaChixのオートセーブ処理によるブラシ遅延問題をほぼ解決。PCでのテストで保存処理中かどうかわからない程度に。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.131.1';
-const POTI_LOT = 'lot.20260206';
+const POTI_VER = 'v6.132.0';
+const POTI_LOT = 'lot.20260208';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
