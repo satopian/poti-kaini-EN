36b2261b41735cadba0fea7929b1fd330bda45b3
satopian <satopian@mia.eek.jp>
2026-02-09 12:29:25 +0900

    litaChix更新。chiファイルの圧縮Libraryをpakoからfflateに変更してオートセーブを高速化。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.132.0';
-const POTI_LOT = 'lot.20260208';
+const POTI_VER = 'v6.133.0';
+const POTI_LOT = 'lot.20260209';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
