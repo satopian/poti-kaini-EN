384a8b2cd5520749487b910794d22a4498871ed0
satopian <satopian@mia.eek.jp>
2026-02-14 16:00:48 +0900

    litaChixの筆圧ローパスフィルタを調整。最初の1タップにはフィルタをかけない。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.136.1';
-const POTI_LOT = 'lot.20260212';
+const POTI_VER = 'v6.136.2';
+const POTI_LOT = 'lot.20260214';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
