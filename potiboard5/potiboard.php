1d48d49735ce2bac98b78ef1a7906b3eb30a59e6
satopian <satopian@mia.eek.jp>
2026-02-16 22:28:59 +0900

    litaChix更新。yieldToMainをMessageChannel()にして高速化

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.137.1';
-const POTI_LOT = 'lot.20260216';
+const POTI_VER = 'v6.137.2';
+const POTI_LOT = 'lot.20260217';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
