090e0abaf637b4ed0f5da88098a11b2021ecb1ea
satopian <satopian@mia.eek.jp>
2026-02-19 23:33:52 +0900

    litaChix更新。保存処理を高速化。新機能｢線の色を描画色に変更｣フィルタを追加。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.137.2';
-const POTI_LOT = 'lot.20260217';
+const POTI_VER = 'v6.138.0';
+const POTI_LOT = 'lot.20260219';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
