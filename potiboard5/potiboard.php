5ae74295378e0b409510579151769fe946feab9b
satopian <satopian@mia.eek.jp>
2026-02-23 13:48:26 +0900

    ファイルメニューからPNGまたはZIPでダウンロードした時に、ブラウザストレージのバックアップが消えるバグを修正。

--- potiboard5/potiboard.php
+++ potiboard5/potiboard.php
@@ -3,8 +3,8 @@
 
 // POTI-board EVO
 // バージョン :
-const POTI_VER = 'v6.150.0';
-const POTI_LOT = 'lot.20260222';
+const POTI_VER = 'v6.150.1';
+const POTI_LOT = 'lot.20260223';
 
 /*
   (C) 2018-2025 POTI改 POTI-board redevelopment team
